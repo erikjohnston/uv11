caac06a536fcf5eb0f03ee34c3a4b9f70e64839c
Erik Johnston <erikj@jki.re>
2015-01-02 10:30:25 +0000

    Fix ADL lookups for free begin() and end() functions for Buffer type

--- include/uv11/buffer.hh
+++ include/uv11/buffer.hh
@@ -10,41 +10,24 @@
 
 namespace uv11 {
     using ::uv_buf_t;
-    using Buffer = uv_buf_t;
+
+    struct Buffer : public uv_buf_t {
+        Buffer();
+        Buffer(uv_buf_t const& t);
+        Buffer(uv_buf_t && t);
+    };
 
     Buffer make_buffer(std::vector<char>&);
     Buffer make_buffer(char*, unsigned int);
 
     // Allow people to iterate over uv_buf_t. These are found via ADL.
-    template<typename B, typename = typename std::enable_if<std::is_same<B, Buffer>::value>::type >
-    char* begin(B& buf) {
-        return buf.base;
-    }
+    char* begin(Buffer& buf);
+    char const* begin(Buffer const& buf);
+    char const* cbegin(Buffer& buf);
 
-    template<typename B, typename = typename std::enable_if<std::is_same<B, Buffer>::value>::type >
-    char const* begin(B& buf) {
-        return buf.base;
-    }
-
-    template<typename B, typename = typename std::enable_if<std::is_same<B, Buffer>::value>::type >
-    char const* cbegin(B& buf) {
-        return buf.base;
-    }
-
-    template<typename B, typename = typename std::enable_if<std::is_same<B, Buffer>::value>::type >
-    char* end(B& buf) {
-        return buf.base + buf.len;
-    }
-
-    template<typename B, typename = typename std::enable_if<std::is_same<B, Buffer>::value>::type >
-    char const* end(B& buf) {
-        return buf.base + buf.len;
-    }
-
-    template<typename B, typename = typename std::enable_if<std::is_same<B, Buffer>::value>::type >
-    char const* cend(B& buf) {
-        return buf.base + buf.len;
-    }
+    char* end(Buffer& buf);
+    char const* end(Buffer const& buf);
+    char const* cend(Buffer& buf);
 }
 
 
--- src/buffer.cpp
+++ src/buffer.cpp
@@ -8,3 +8,31 @@ Buffer uv11::make_buffer(char * base, unsigned int len) {
 Buffer uv11::make_buffer(std::vector<char>& v) {
     return ::uv_buf_init(v.data(), v.size());
 }
+
+Buffer::Buffer() {}
+Buffer::Buffer(uv_buf_t const &t) : uv_buf_t(t) {}
+Buffer::Buffer(uv_buf_t &&t) : uv_buf_t(std::move(t)) {}
+
+char* uv11::begin(Buffer& buf) {
+    return buf.base;
+}
+
+char const* uv11::begin(Buffer const& buf) {
+    return buf.base;
+}
+
+char const* uv11::cbegin(Buffer& buf) {
+    return buf.base;
+}
+
+char* uv11::end(Buffer& buf) {
+    return buf.base + buf.len;
+}
+
+char const* uv11::end(Buffer const& buf) {
+    return buf.base + buf.len;
+}
+
+char const* uv11::cend(Buffer& buf) {
+    return buf.base + buf.len;
+}
